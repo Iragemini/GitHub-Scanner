998db6950cdb70e2bef10411b19481abe11f7fdf
Irina Karavaichyk <irinakaravaichyk@mac-1.home>
2025-04-21 20:30:04 +0200

    feat: add github service and octokit provider

---
+++ src/github/octokit.provider.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { Octokit } from '@octokit/rest';
+import { Provider } from '@nestjs/common';
+
+export const OCTOKIT = 'OCTOKIT';
+
+export const OctokitProvider: Provider = {
+  provide: OCTOKIT,
+  useFactory: (configService: ConfigService) => {
+    const token = configService.get<string>('GITHUB_TOKEN');
+    return new Octokit({ auth: token });
+  },
+  inject: [ConfigService],
+};
