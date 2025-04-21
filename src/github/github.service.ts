bf2a5879457676447d78c7ec81ca01f914b411b1
Irina Karavaichyk <irinakaravaichyk@mac-1.home>
2025-04-21 21:34:50 +0200

    feat: add getRepoDetails function

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,9 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { OCTOKIT, OctokitInstance, GitHubRepo } from './octokit.provider';
+import { GITHUB_IDENTITY, GitHubIdentity } from './github-identity.provider';
 
 @Injectable()
 export class GithubService {
-  constructor(@Inject(OCTOKIT) private readonly octokit: OctokitInstance) {}
+  constructor(
+    @Inject(OCTOKIT) private readonly octokit: OctokitInstance,
+    @Inject(GITHUB_IDENTITY) private readonly identity: GitHubIdentity,
+  ) {}
 
   private mapRepoInfo(repo: GitHubRepo) {
     return {
@@ -28,5 +32,21 @@ export class GithubService {
     });
 
     return data.map(this.mapRepoInfo);
+  }
+
+  async getRepoDetails(repoName: string) {
+    const userName = this.identity.login;
+
+    const { data: repo } = await this.octokit.rest.repos.get({
+      owner: userName,
+      repo: repoName,
+    });
+
+    return {
+      name: repo.name,
+      size: repo.size,
+      owner: repo.owner.login,
+      isPrivate: repo.private,
+    };
   }
 }
