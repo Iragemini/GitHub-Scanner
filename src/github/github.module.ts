2d2708e4ae74d39b06a546f437b6cf45a87f40ff
Irina Karavaichyk <irinakaravaichyk@mac-1.home>
2025-04-21 21:25:39 +0200

    feat: add identity provider

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OctokitProvider } from './octokit.provider';
 import { GithubService } from './github.service';
+import { GitHubIdentityProvider } from './github-identity.provider';
 
 @Module({
   imports: [ConfigModule],
-  providers: [OctokitProvider, GithubService],
+  providers: [OctokitProvider, GithubService, GitHubIdentityProvider],
   exports: [GithubService],
 })
 export class GithubModule {}
